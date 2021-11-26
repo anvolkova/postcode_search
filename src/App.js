79fec1bb426fa6e3ad311861570c6826c39f0d31
Anna Volkova <annettavo@gmail.com>
2021-11-26 11:55:00 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, {useState} from "react";
+import './App.css';
+import SearchForm from "./SearchForm";
+
+export default function App() {
+  // const [locations, setLocations] = useState();
+  const [postcode, setPostcode] = useState("");
+  const [suburb, setSuburb] = useState("");
+  const [state, setState] = useState("");
+  const [validation, setValidation] = useState("");
+  const [message, setMessage] = useState("");
+
+  async function postcodeSearch(q){
+    try{
+      const result = await fetch(`https://digitalapi.auspost.com.au/postcode/search.json?q=${encodeURIComponent(q)}&excludePostBoxFlag=true`, {
+        "method": "GET",
+        "headers": {
+          "AUTH-KEY": "872608e3-4530-4c6a-a369-052accb03ca8"
+        }
+      });
+      const data = (await result.json()).localities.locality;
+      if (data) {
+        if(!Array.isArray(data)){
+          return [data];
+        } else {
+          return data;
+        }
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleFormSubmit(event){
+    event.preventDefault();
+
+    setValidation("");
+    setMessage("");
+
+    if(!postcode) {
+      setValidation("Please enter a postcode");
+      return ;
+    }
+    if(!postcode.match(/^[0-9]{4}$/)) {
+      setValidation("Please enter 4-digit postcode");
+      return ;
+    }
+    if(!suburb) {
+      setValidation("Please enter a suburb name");
+      return ;
+    }
+    if(!state) {
+      setValidation("Please enter a state");
+      return ;
+    }
+
+    const locations = await postcodeSearch(postcode);
+
+    const suburbUpperCase = suburb.toUpperCase();
+    var foundSuburb = false;
+    var foundState = false;
+
+    for(var i in locations) {
+      const location = locations[i];
+      if(location.location === suburbUpperCase) {
+        foundSuburb = true;
+        if (location.state === state) {
+          foundState = true;
+          break;
+        }
+      }
+    }
+    
+    if(!foundSuburb) {
+      setValidation(`The postcode ${postcode} does not match the suburb ${suburb}`);
+    } else if(!foundState) {
+      setValidation(`The suburb ${suburb} with postcode ${postcode} does not exist in the state ${state}`);
+    } else {
+      setValidation("The postcode, suburb and state entered are valid");
+    }
+  }
+
+  return (<>
+    <div className="App">
+      <header className="App-header">Welcome to Australia Postcodes</header>
+      <SearchForm 
+              postcode={postcode}
+              onPostcodeChange={e => setPostcode(e.target.value)}
+              suburb={suburb}
+              onSuburbChange={e => setSuburb(e.target.value)}
+              state={state}
+              onStateChange={e => setState(e.target.value)}
+              validation={validation}
+              message={message}
+              onFormSubmit={handleFormSubmit} />
+    </div>
+  </>);
+}
---
+++ src/SearchForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import './index.css';
+
+export default function SearchForm(props){
+    return <form onSubmit={props.onFormSubmit}>
+            <div>
+                <label htmlFor="postcode">Postcode: </label>
+                <input type="text" id="postcode" className="textfield" placeholder="2000" value={props.postcode} onChange={props.onPostcodeChange} />
+            </div>
+            <div>
+                <label htmlFor="suburb">Suburb: </label>
+                <input type="text" id="suburb" className="textfield" placeholder="Sydney" value={props.suburb} onChange={props.onSuburbChange} />
+            </div>
+            <div>
+                <label htmlFor="state">State: </label>
+                <select id="state" value={props.state} onChange={props.onStateChange}>
+                <option value="">Choose a state</option>
+                <option value="NSW">NSW</option>
+                <option value="VIC">VIC</option>
+                <option value="TAS">TAS</option>
+                <option value="SA">SA</option>
+                <option value="WA">WA</option>
+                <option value="QLD">QLD</option>
+                <option value="NT">NT</option>
+                <option value="ACT">ACT</option>
+            </select>
+            </div>
+            <div className="form-footer">
+                <input type="submit" className="btn btn-primary" value="Submit" />
+            </div>
+            <div className="validation-message">{props.validation}</div>
+            <div>{props.message}</div>
+        </form>
+}
